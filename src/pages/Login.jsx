82ba6255de5653f5c2e23fa7a798ce84bc67cde0
Matheus Biaggio <matheusbiaggio@gmail.br>
2022-11-05 11:38:02 -0300

    termino do exercicio 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Link, Redirect, Switch } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends Component {
   state = {
     formName: '',
     isSaveButtonDisabled: true,
+    done: true,
   };
 
   // Valida se o input do formName tem mais do que 3 characters
@@ -37,39 +40,55 @@ class Login extends Component {
   };
 
   onSaveButtonClick = async () => {
-    const newUser = await createUser();
-    console.log(newUser);
+    const { formName, done } = this.state;
+    const { history } = this.props;
+
+    this.setState({
+      done: false,
+    });
+    const newUser = await createUser({ name: formName });
+    this.setState({
+      done: true,
+    });
+    console.log(newUser, done);
+    return (history.push('/search'));
   };
 
   render() {
     const {
       formName,
       isSaveButtonDisabled,
+      done,
     } = this.state;
     return (
       <div data-testid="page-login">
-        Login
-        <form>
-          <label htmlFor="formName">
-            <input
-              data-testid="login-name-input"
-              type="text"
-              placeholder="Usuário"
-              id="formName"
-              name="formName"
-              value={ formName }
-              onChange={ this.onInputChange }
-            />
-          </label>
-          <button
-            data-testid="login-submit-button"
-            type="button"
-            disabled={ isSaveButtonDisabled }
-            onClick={ this.onSaveButtonClick }
-          >
-            Entrar
-          </button>
-        </form>
+        {
+          done
+            ? (
+              <form>
+                <label htmlFor="formName">
+                  <input
+                    data-testid="login-name-input"
+                    type="text"
+                    placeholder="Usuário"
+                    id="formName"
+                    name="formName"
+                    value={ formName }
+                    onChange={ this.onInputChange }
+                  />
+                </label>
+                <button
+                  data-testid="login-submit-button"
+                  type="button"
+                  disabled={ isSaveButtonDisabled }
+                  onClick={ this.onSaveButtonClick }
+                >
+                  Entrar
+                </button>
+              </form>)
+            : <Loading />
+        }
+
       </div>
     );
   }
